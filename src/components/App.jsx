ed0c60695276067a0acf3b6121113982543ed8aa
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-07 18:30:43 +0300

    create app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
+import React, { Suspense } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+const Home = React.lazy(() => import('./components/Home'));
+const Movies = React.lazy(() => import('./components/Movies'));
+const MovieDetails = React.lazy(() => import('./components/MovieDetails'));
+const Cast = React.lazy(() => import('./components/Cast'));
+const Reviews = React.lazy(() => import('./components/Reviews'));
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/movies" component={Movies} />
+          <Route exact path="/movies/:movieId" component={MovieDetails} />
+          <Route exact path="/movies/:movieId/cast" component={Cast} />
+          <Route exact path="/movies/:movieId/reviews" component={Reviews} />
+          <Redirect to="/" />
+        </Switch>
+      </Suspense>
+    </Router>
   );
-};
+}
+
+export default App;
