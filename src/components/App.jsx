64246ff9e96d2943a818b66e097ad0f6f25cb84f
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-08 13:46:23 +0300

    edit code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
-import { Routes, Route, NavLink } from 'react-router-dom';
-import styled from 'styled-components';
+import { Routes, Route } from 'react-router-dom';
+import { Container, Header, Logo, Link } from './App.styled';
 import Home from '../pages/Home';
 import Movies from '../pages/Movies';
 import MovieDetails from '../pages/MovieDetails';
 import Cast from '../pages/Cast';
 import Reviews from '../pages/Reviews';
 
-const StyledLink = styled(NavLink)`
-  color: black;
-
-  &.active {
-    color: orange;
-  }
-`;
-
 function App() {
   return (
-    <div>
-      <nav>
-        <StyledLink to="/" end>
-          Home
-        </StyledLink>
-        <StyledLink to="/movies">Movies</StyledLink>
-      </nav>
+    <Container>
+      <Header>
+        <Logo>
+          <span role="img" aria-label="computer icon">
+            🎞️
+          </span>{' '}
+          The movie theater
+        </Logo>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </Header>
 
       <Routes>
         <Route path="/" element={<Home />} />
@@ -32,7 +32,7 @@ function App() {
         <Route path="/movies/:movieId/cast" element={<Cast />} />
         <Route path="/movies/:movieId/reviews" element={<Reviews />} />
       </Routes>
-    </div>
+    </Container>
   );
 }
 
---
+++ src/components/App.styled.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+export const Container = styled.div`
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 0 16px;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 12px;
+  padding: 8px 0;
+  margin-bottom: 16px;
+  border-bottom: 1px solid black;
+
+  > nav {
+    display: flex;
+  }
+`;
+
+export const Logo = styled.p`
+  font-weight: 700;
+  margin: 0;
+`;
+
+export const Link = styled(NavLink)`
+  padding: 8px 16px;
+  border-radius: 4px;
+  text-decoration: none;
+  color: black;
+  font-weight: 500;
+
+  &.active {
+    color: white;
+    background-color: orangered;
+  }
+`;
