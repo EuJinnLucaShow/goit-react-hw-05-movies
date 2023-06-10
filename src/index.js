ce229099cf5a3ee5ca5f7e06e02d6cf365b2ac62
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-10 23:08:08 +0300

    edit cide

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
     </BrowserRouter>
   </React.StrictMode>
