fe5fac3b13f34988b2565e062006f69e6d6d8e8e
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-10 22:18:49 +0300

    edit code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Container, Header, Logo, Link } from './App.styled';
 
 // Import components
 import Home from '../pages/Home';
@@ -10,7 +11,21 @@ import Reviews from '../pages/Reviews';
 
 const App = () => {
   return (
-    <>
+    <Container>
+      <Header>
+        <Logo>
+          <span role="img" aria-label="computer icon">
+            🎞️
+          </span>{' '}
+          Movie Search
+        </Logo>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/movies">Movie</Link>
+        </nav>
+      </Header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
@@ -19,7 +34,7 @@ const App = () => {
         <Route path="/movies/:movieId/reviews" element={<Reviews />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </>
+    </Container>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,19 @@ import axios from 'axios';
 
 const Home = () => {
   const [trendingMovies, setTrendingMovies] = useState([]);
+  const API_KEY = 'a4e0e6c94492c515df52f4a6ebcc54c7';
+  axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
   useEffect(() => {
     // Отримання списку популярних фільмів з API
+    const params = {
+      params: {
+        api_key: API_KEY,
+      },
+    };
     const fetchTrendingMovies = async () => {
       try {
-        const response = await axios.get('/trending/get-trending', {
-          params: {
-            apiKey: 'a4e0e6c94492c515df52f4a6ebcc54c7',
-          },
-        });
+        const response = await axios.get(`trending/movie/day`, params);
         setTrendingMovies(response.data.results);
       } catch (error) {
         console.error(error);
