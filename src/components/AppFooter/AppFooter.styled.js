757fc00dd0ff8feb6c12757877eae92034ae2572
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-12 23:03:46 +0300

    edit code

--- src/components/AppFooter/AppFooter.styled.js
+++ src/components/AppFooter/AppFooter.styled.js
@@ -2,13 +2,6 @@ import styled from 'styled-components';
 
 export const Footer = styled.footer`
   border-top: 1px solid #ececec;
-  margin-top: 1rem;
-  padding: 1rem;
-  background-color: rgb(235, 195, 64);
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
 `;
 
 export const Text = styled.p`
